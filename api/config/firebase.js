dc7705662d0ada1d18f438f7788933be23688cc2
Himanshu Sharma <SharmaHimanshu030700@gmail.com>
2024-04-11 08:43:29 +0530

    commit api setup

---
+++ api/config/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase/app';
+import { } from 'fire'
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    //...
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
